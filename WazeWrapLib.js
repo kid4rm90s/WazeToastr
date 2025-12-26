89e911c233eee16fe6ab40415e48850611b7cb31
kid4rm90s <165776943+kid4rm90s@users.noreply.github.com>
2025-12-26 20:57:33 +0800

    Update WazeWrapLib.js

--- WazeWrapLib.js
+++ WazeWrapLib.js
@@ -243,15 +243,15 @@
             }
             loadSettings();
             $('head').append(
-                $('<link/>', {
-                    rel: 'stylesheet',
-                    type: 'text/css',
-                    href: 'https://cdn.statically.io/gh/WazeDev/toastr/master/build/toastr.min.css'
-                }),
-                $('<style type="text/css">.toast-container-wazedev > div {opacity: 0.95;} .toast-top-center-wide {top: 32px;}</style>')
+              $('<link/>', {
+                rel: 'stylesheet',
+                type: 'text/css',
+                href: 'https://cdn.jsdelivr.net/gh/WazeDev/toastr@master/build/toastr.min.css',
+              }),
+              $('<style type="text/css">.toast-container-wazedev > div {opacity: 0.95;} .toast-top-center-wide {top: 32px;}</style>')
             );
 
-            await $.getScript('https://cdn.statically.io/gh/WazeDev/toastr/master/build/toastr.min.js');
+            await $.getScript('https://cdn.jsdelivr.net/gh/WazeDev/toastr@master/build/toastr.min.js');
 		wazedevtoastr.options = {
 		    target: '#map',
 		    timeOut: 6000,
